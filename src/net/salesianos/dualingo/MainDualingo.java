d2ec76c41567c6dc18dc170a0fb0059847fce7bd
the-albertox <usr3562@salesianos-lacuesta.net>
2023-05-16 09:51:26 +0100

    feat : created menu

---
+++ src/net/salesianos/dualingo/MainDualingo.java
@@ -0,0 +1,27 @@
+package net.salesianos.dualingo;
+
+import java.util.Scanner;
+
+import net.salesianos.utiles.ClasificadorDePalabras;
+
+public class MainDualingo{
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+        ClasificadorDePalabras clasificador = new ClasificadorDePalabras();
+
+        System.out.println("Bienvenido al clasificador de palabras.");
+
+        while (true) {
+            System.out.println("\nMenu:");
+            System.out.println("1. Añadir palabra");
+            System.out.println("2. Eliminar palabra");
+            System.out.println("3. Existe palabra");
+            System.out.println("4. Mostrar iniciales disponibles");
+            System.out.println("5. Ver palabras por inicial");
+            System.out.println("6. Cerrar programa");
+
+            int option = scanner.nextInt();
+            scanner.nextLine();
+}
+}
+};
